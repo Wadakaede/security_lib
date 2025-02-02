295e3df322f3fba81c5971f7d6abc9c9db18773f
Wadakaede <kaede04079642@outlook.com>
2025-02-02 12:47:49 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
     name="security_lib",  # パッケージ名
     version="1.0.0",  # バージョン
     description="A custom security library with hashing, encryption, and network tools",
-    author="Your Name",
-    author_email="your.email@example.com",
-    url="https://github.com/yourusername/security_lib",  # リポジトリがあれば
+    author="Wadakaede",
+    author_email="kaede04079642@outlook.com",
+    url="https://github.com/Wadakaede/security_lib.git",  # リポジトリがあれば
     packages=find_packages(),  # パッケージ自動検出
     install_requires=[
         "cryptography>=3.4.8"  # 必要な依存ライブラリ
